c486766bd2924785028c61de0481bd49f8678fa9
Nashira Deer <nashiradeer@gmail.com>
2024-07-09 17:33:16 +0000

    feat: add Elixir functions to the NIFs

--- lib/simple_rijndael.ex
+++ lib/simple_rijndael.ex
@@ -3,5 +3,9 @@ defmodule SimpleRijndael do
     otp_app: :simple_rijndael,
     crate: :simple_rijndael_nif
 
-  def add(_arg1, _arg2), do: :erlang.nif_error(:nif_not_loaded)
+  def init_cbc_mode(_key, _block_size, _padding), do: :erlang.nif_error(:nif_not_loaded)
+
+  def decrypt(_cipher, _iv, _data), do: :erlang.nif_error(:nif_not_loaded)
+
+  def encrypt(_cipher, _iv, _data), do: :erlang.nif_error(:nif_not_loaded)
 end
