d769b58e5866c48adcfdc7c7b1d4a6d55ffa4d30
Nashira Deer <nashiradeer@gmail.com>
2024-07-10 19:53:52 +0000

    feat: document and error handling

--- lib/simple_rijndael.ex
+++ lib/simple_rijndael.ex
@@ -1,11 +1,37 @@
 defmodule SimpleRijndael do
+  @moduledoc """
+  Wrapper module to interact with the Rust NIF for the `simple_rijndael` Rust crate.
+  """
+
+  @typedoc """
+  The error types that can be returned by the NIF.
+  """
+  @type error :: :invalid_data_size | :invalid_block_size | :invalid_key_size
+
+  @typedoc """
+  The padding types that can be used for the Rijndael cipher.
+  """
+  @type padding :: :pkcs7 | :zero
+
   use Rustler,
     otp_app: :simple_rijndael,
     crate: :simple_rijndael_nif
 
+  @doc """
+  Initializes the low-level Rijndael struct in CBC mode with the given key, block size (in bytes), and padding.
+  """
+  @spec init_cbc_mode(binary(), non_neg_integer(), padding()) :: {:ok, reference()} | {:error, error()}
   def init_cbc_mode(_key, _block_size, _padding), do: :erlang.nif_error(:nif_not_loaded)
 
+  @doc """
+  Decrypts the given data using the Rijndael cipher and the given initialization vector.
+  """
+  @spec decrypt(reference(), binary(), binary()) :: {:ok, binary()} | {:error, error()}
   def decrypt(_cipher, _iv, _data), do: :erlang.nif_error(:nif_not_loaded)
 
+  @doc """
+  Encrypts the given data using the Rijndael cipher and the given initialization vector.
+  """
+  @spec encrypt(reference(), binary(), binary()) :: {:ok, binary()} | {:error, error()}
   def encrypt(_cipher, _iv, _data), do: :erlang.nif_error(:nif_not_loaded)
 end
--- native/simple_rijndael_nif/src/lib.rs
+++ native/simple_rijndael_nif/src/lib.rs
@@ -1,89 +1,124 @@
+//! A NIF library for [simple_rijndael] crate.
+
 use std::{io::Write, ops::Deref};
 
-use rustler::{Binary, Env, NifUnitEnum, OwnedBinary, ResourceArc, Term};
+use rustler::{types::atom, Atom, Binary, Env, Error, NifUnitEnum, OwnedBinary, ResourceArc, Term};
 use simple_rijndael::{
     impls::RijndaelCbc,
     paddings::{Pkcs7Padding, ZeroPadding},
+    Errors,
 };
 
+mod atoms {
+    //! Atoms for the NIF library.
+
+    rustler::atoms! {
+        invalid_data_size,
+        invalid_block_size,
+        invalid_key_size,
+    }
+}
+
+/// Padding for [Rijndael] cipher.
 #[derive(NifUnitEnum)]
 enum RijndaelPadding {
+    /// PKCS7 padding.
     Pkcs7,
+    /// Zero padding.
     Zero,
 }
 
+/// A enum wrapping [RijndaelCbc], because the padding is set by generics and [rustler] doesn't support generics.
 enum Rijndael {
+    /// Rijndael with PKCS7 padding, wraps [RijndaelCbc] with [Pkcs7Padding].
     CbcPkcs7(RijndaelCbc<Pkcs7Padding>),
+    /// Rijndael with Zero padding, wraps [RijndaelCbc] with [ZeroPadding].
     CbcZero(RijndaelCbc<ZeroPadding>),
 }
 
+/// Executes when the NIF library is loaded.
 fn load(env: Env, _: Term) -> bool {
-    rustler::resource!(Rijndael, env);
+    _ = rustler::resource!(Rijndael, env);
     true
 }
 
+/// Converts an [Errors] to an [Atom].
+fn error_to_atom(error: Errors) -> Atom {
+    match error {
+        Errors::InvalidDataSize => atoms::invalid_data_size(),
+        Errors::InvalidBlockSize => atoms::invalid_block_size(),
+        Errors::InvalidKeySize => atoms::invalid_key_size(),
+    }
+}
+
+/// Wraps an [Errors] from [simple_rijndael] into a [Error] from [rustler].
+fn wrap_error(error: Errors) -> Error {
+    Error::Term(Box::new(error_to_atom(error)))
+}
+
+/// Initializes a [Rijndael] cipher with CBC mode.
 #[rustler::nif]
 fn init_cbc_mode(
     key: Binary,
     block_size: usize,
     padding: RijndaelPadding,
-) -> ResourceArc<Rijndael> {
+) -> Result<(Atom, ResourceArc<Rijndael>), Error> {
     let cipher = match padding {
         RijndaelPadding::Pkcs7 => {
-            Rijndael::CbcPkcs7(RijndaelCbc::new(key.as_slice(), block_size).unwrap())
+            Rijndael::CbcPkcs7(RijndaelCbc::new(key.as_slice(), block_size).map_err(wrap_error)?)
         }
         RijndaelPadding::Zero => {
-            Rijndael::CbcZero(RijndaelCbc::new(key.as_slice(), block_size).unwrap())
+            Rijndael::CbcZero(RijndaelCbc::new(key.as_slice(), block_size).map_err(wrap_error)?)
         }
     };
 
-    ResourceArc::new(cipher)
+    Ok((atom::ok(), ResourceArc::new(cipher)))
 }
 
+/// Decrypts data with a [Rijndael] cipher.
 #[rustler::nif]
 fn decrypt<'a>(
     env: Env<'a>,
     cipher: ResourceArc<Rijndael>,
     iv: Binary<'a>,
     data: Binary<'a>,
-) -> Binary<'a> {
+) -> Result<(Atom, Binary<'a>), Error> {
     let decypted = match cipher.deref() {
         Rijndael::CbcPkcs7(inner) => inner
             .decrypt(iv.as_slice(), Vec::from(data.as_slice()))
-            .unwrap(),
+            .map_err(wrap_error)?,
         Rijndael::CbcZero(inner) => inner
             .decrypt(iv.as_slice(), Vec::from(data.as_slice()))
-            .unwrap(),
+            .map_err(wrap_error)?,
     };
 
     let mut binary = OwnedBinary::new(decypted.len()).unwrap();
     _ = binary.as_mut_slice().write_all(&decypted);
-    binary.release(env)
+
+    Ok((atom::ok(), binary.release(env)))
 }
 
+/// Encrypts data with a [Rijndael] cipher.
 #[rustler::nif]
 fn encrypt<'a>(
     env: Env<'a>,
     cipher: ResourceArc<Rijndael>,
     iv: Binary<'a>,
     data: Binary<'a>,
-) -> Binary<'a> {
+) -> Result<(Atom, Binary<'a>), Error> {
     let encrypted = match cipher.deref() {
         Rijndael::CbcPkcs7(inner) => inner
             .encrypt(iv.as_slice(), Vec::from(data.as_slice()))
-            .unwrap(),
+            .map_err(wrap_error)?,
         Rijndael::CbcZero(inner) => inner
             .encrypt(iv.as_slice(), Vec::from(data.as_slice()))
-            .unwrap(),
+            .map_err(wrap_error)?,
     };
 
     let mut binary = OwnedBinary::new(encrypted.len()).unwrap();
     _ = binary.as_mut_slice().write_all(&encrypted);
-    binary.release(env)
+
+    Ok((atom::ok(), binary.release(env)))
 }
 
-rustler::init!(
-    "Elixir.SimpleRijndael",
-    [init_cbc_mode, decrypt, encrypt],
-    load = load
-);
+rustler::init!("Elixir.SimpleRijndael", load = load);
