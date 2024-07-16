5fa690185c7b96db419bcf939aadd45bfbf0e13b
Nashira Deer <nashiradeer@gmail.com>
2024-07-16 00:57:32 +0000

    build(mix): configure package and add ex_doc

--- mix.exs
+++ mix.exs
@@ -1,25 +1,53 @@
 defmodule SimpleRijndael.MixProject do
   use Mix.Project
 
+  @source_url "https://github.com/nashiradeer/simple-rijndael-4elixir"
+  @version "0.1.0"
+
   def project do
     [
       app: :simple_rijndael,
-      version: "0.1.0",
+      name: "SimpleRijndael",
+      version: @version,
       elixir: "~> 1.17",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      package: package(),
+      docs: docs()
     ]
   end
 
   def application do
-    [
-      extra_applications: [:logger]
-    ]
+    [extra_applications: [:logger, :eex]]
   end
 
   defp deps do
     [
-       {:rustler, "~> 0.34.0", runtime: false}
+      {:ex_doc, "~> 0.31", only: :dev, runtime: false},
+      {:rustler, "~> 0.34.0", runtime: false}
+    ]
+  end
+
+  defp package do
+    [
+      description: "A simple Rijndael implementation for Elixir.",
+      files: ["lib", "mix.exs", "README.md", "LICENSE.txt", "native", "test"],
+      maintainers: ["Nashira Deer"],
+      licenses: ["MIT"],
+      links: %{
+        "GitHub" => "https://github.com/nashiradeer/simple-rijndael-4elixir"
+      }
+    ]
+  end
+
+  defp docs do
+    [
+      extras: ["README.md", "LICENSE.txt"],
+      main: "readme",
+      homepage_url: @source_url,
+      source_url: @source_url,
+      source_url_pattern: "#{@source_url}/tree/v#{@version}/lib/%{file}#L%{line}",
+      formatters: ["html"]
     ]
   end
 end
